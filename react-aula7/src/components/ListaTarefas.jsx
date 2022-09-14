e55dd7725c27aa276434af6ad26c460884acc8a5
FranklinPdon <franklinpdon@gmail.com>
2022-09-14 20:55:08 -0300

    Components

---
+++ react-aula7/src/components/ListaTarefas.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Tarefa from './Tarefa'
+import { DivLista } from '../style/styled'
+
+export default function ListaTarefas () {
+  return (
+    <DivLista>
+        <p>Lista de tarefas</p>
+        <Tarefa/>
+        <Tarefa/>
+        <Tarefa/>
+    </DivLista>
+  )
+}
---
+++ react-aula7/src/components/Tarefa.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import styled from 'styled-components'
+
+//CRAINDO O MODULO DE ESTILOS
+const DivTarefa = styled.div`
+    background-color: #ffb;
+    border: 2px solid #333;
+    box-shadow: 5px 5px 5px #333;
+    padding: 20px;
+    text-align: center;
+    width: 350px;
+    height: 200px;
+
+    h2,p{
+        padding-bottom: 10px;
+    }
+    
+`
+
+export default function Tarefa() {
+  return (
+    <DivTarefa>
+        <h2>Relatorio de vendas</h2>
+        <p>Para: Departamento de vendas</p>
+        <p>Levantar os valores das vendas desse mês.</p>
+    </DivTarefa>
+  )
+}
