ddee1863e3d20e13a8a0c403d043c16ad863962d
mouragus <gumora18@hotmail.com>
2022-09-14 20:42:21 -0300

    App Criado com Styled.js

--- rm94280/react-aula7/src/App.jsx
+++ rm94280/react-aula7/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import ListaTarefas from './components/ListaTarefas'
 
 export default function App() {
   return (
     <div>
         <h1>Estudo sobre styled-components</h1>
+        <ListaTarefas/>
     </div>
   )
 }
---
+++ rm94280/react-aula7/src/components/Tarefa.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { DivTarefa } from '../style/styled'
+
+export default function Tarefa() {
+  return (
+    <DivTarefa>
+        <h2>Relatorio de Vendas</h2>
+        <p>Para : Departamento de vendas</p>
+        <p>Levantar os valores desse mês</p>
+    </DivTarefa>
+  )
+}
