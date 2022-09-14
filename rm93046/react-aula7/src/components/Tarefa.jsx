00d946f90a93c96209c80f06a037cd209e9c6ab9
Nakamura16 <andre.massao.nakamura@gmail.com>
2022-09-14 20:22:33 -0300

    Tarefa.jsx created and imported on ListaTarefa.jsx

---
+++ rm93046/react-aula7/src/components/Tarefa.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Tarefa() {
+  return (
+    <div>
+        <h2>Relatório de vendas</h2>
+        <p>Para: departamento de vendas.</p>
+        <p>Levantar os valores das vendas desse mês</p>
+    </div>
+  )
+}
