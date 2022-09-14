8b5a26ee98862c525231309104dc27b542016ef1
doguebart <douglasweelber.2004@outlook.com>
2022-09-14 20:55:25 -0300

    useState added

--- rm94269/react-aula7/src/components/listaTarefas/ListaTarefas.jsx
+++ rm94269/react-aula7/src/components/listaTarefas/ListaTarefas.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Tarefa from '../tarefa/Tarefa'
 import { DivLista } from '../styled'
 
 export default function ListaTarefas() {
+  const[tarefa, setTarefa] = useState([
+
+   {
+    titulo: "Lista de pagamento",
+    setor: "Dep. Vendas",
+    descrição: "Levantar os pagamentos das vendas"
+  },
+  {
+    titulo: "Planilha de salários",
+    setor: "Dep. Pessoal",
+    descrição: "Gerar planilha"
+  }
+])
+
+
   return (
     <DivLista>
         <p>Lista de tarefas</p>
