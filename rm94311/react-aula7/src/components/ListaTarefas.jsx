12e7533498eb6dd1cd81dbb4a9dac2f095a2c274
M4rcosHBS <marcosbilobram@hotmail.com>
2022-09-14 20:22:36 -0300

    Tarefa.jsx criado

--- rm94311/react-aula7/src/components/ListaTarefas.jsx
+++ rm94311/react-aula7/src/components/ListaTarefas.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Tarefa from './Tarefa'
 
 export default function ListaTarefas() {
   return (
     <div>
         <p>Lista de tarefas</p>
+        <Tarefa/>
+        <Tarefa/>
+        <Tarefa/>
     </div>
   )
 }
