83ed1647774ea86e45eac3599d13eda90473ce58
rm94233@fiap.com.br <rm94233@fiap.com.br>
2022-09-14 20:58:31 -0300

    Styled components

---
+++ rm94233/react-aula7/src/components/ListaTarefas.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Tarefa from './Tarefa'
+
+export default function ListaTarefas() {
+  return (
+    <div>
+        <p>Lista de Tarefas</p>
+        <Tarefa/>
+    </div>
+  )
+}
