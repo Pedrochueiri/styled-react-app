1dce0aabb9789a870b29e0eaf6c9cdc80925a7eb
mouragus <gumora18@hotmail.com>
2022-09-14 21:03:11 -0300

    props useeffect

--- rm94280/react-aula7/src/components/ListaTarefas.jsx
+++ rm94280/react-aula7/src/components/ListaTarefas.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Tarefa from './Tarefa'
 import { DivLista } from '../style/styled'
 
+const [tarefa, setTarefa] = useState([{
+    titulo: 'Lista de Pagamento',
+    setor: 'Dep. Vendas',
+    descricao: 'Levantar os valores das vendas',
+    },
+    {
+        titulo: 'Planilha de Salarios',
+        setor: 'Dep. Pessoal',
+        descricao:'Gerar Planilhas',
+    },
+])
+return(
+    <DivLista>
+        {tarefa.map((tar,i)=>(
+            <Tarefa
+            key={i}
+            titulo={tar.titulo}
+            setor={tar.setor}
+            descricao={tar.descricao}
+            />
+        ))}
+    </DivLista>
+)
 export default function ListaTarefas() {
   return (
     <DivLista>
--- rm94280/react-aula7/src/components/Tarefa.jsx
+++ rm94280/react-aula7/src/components/Tarefa.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { DivTarefa } from '../style/styled'
 
-export default function Tarefa() {
-  return (
-    <DivTarefa>
-        <h2>Relatorio de Vendas</h2>
-        <p>Para : Departamento de vendas</p>
-        <p>Levantar os valores desse mês</p>
-    </DivTarefa>
-  )
+const Tarefa = props=>{
+    return(
+        <DivTarefa>
+            <h2>{props.titulo}</h2>
+            <p>Para: {props.setor}</p>
+            <p>{props.descricao}</p>
+        </DivTarefa>
+    )
 }
+export default Tarefa;
