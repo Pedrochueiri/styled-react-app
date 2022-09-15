f2cdfaece21ea0d1cea9eda1aa2844cc07dde6fd
Logon Aluno <logonrmlocal@fiap.com.br>
2022-09-14 21:01:39 -0300

    Ajuste

--- rm94898/react-aula7/src/components/ListaTarefas.jsx
+++ rm94898/react-aula7/src/components/ListaTarefas.jsx
@@ -1,26 +1,48 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { DivLista } from "./style/styled";
 import Tarefa from "./Tarefa";
 
 const ListaTarefas = () => {
   const [tarefa, setTarefa] = useState([
     {
-      titulo: "lista de pagamento",
+      titulo: "Lista de pagamento",
       setor: "Dep. vendas",
       descricao: "levantar os valroes da vendas",
     },
-    { titulo: "Planilha de salários", setor: 'Dep. Pessoal',
-  descricao: "Gerar planilhas",
-},
+    {
+      titulo: "Planilha de salários",
+      setor: "Dep. Pessoal",
+      descricao: "Gerar planilhas",
+    },
+    {
+      titulo: "Lista de gastos",
+      setor: "Dep. Pessoal",
+      descricao: "Gerar planilhas",
+    },
+    {
+      titulo: "Lista de Relatorios",
+      setor: "Dep. Pessoal",
+      descricao: "Gerar planilhas",
+    },
+    {
+      titulo: "Docie",
+      setor: "Dep. Pessoal",
+      descricao: "Gerar planilhas",
+    },
+    {
+      titulo: "Planilha de Folha",
+      setor: "Dep. Pessoal",
+      descricao: "Gerar planilhas",
+    },
   ]);
   return (
     <DivLista>
       {tarefa.map((tar, i) => (
         <Tarefa
-        key={i}
-        titulo={tar.titulo}
-        setor={tar.setor}
-        descricao={tar.descricao}
+          key={i}
+          titulo={tar.titulo}
+          setor={tar.setor}
+          descricao={tar.descricao}
         />
       ))}
     </DivLista>
