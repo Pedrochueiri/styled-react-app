bb20bc89ed97b334ebd338827d5cdf095699bc36
matlencar <rm94642@fiap.com.br>
2022-09-14 21:07:22 -0300

    styled-components

--- rm94642/aula06/src/components/ListaTarefas.jsx
+++ rm94642/aula06/src/components/ListaTarefas.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import Tarefa from './Tarefa'
 import { DivLista } from './style/styled'
 
 
 export default function ListaTarefas() {
+
+    const [tarefa, setTarefa] = useState([{
+        titulo: 'Lista de pagamento',
+        setor: 'Dep. Vendas',
+        descricao: 'Levantar os valores das vendas',
+    },
+    {
+        titulo: 'Planilha de salario',
+        setor: 'Dep. Pessoal',
+        descricao: 'Gerar planilhas',
+    }
+])
   return (
     <DivLista>
-        <Tarefa/>
-        <Tarefa/>
-        <Tarefa/>
-    </DivLista>
+            {tarefa.map((tar, i)=>(
+                <Tarefa
+                key={i}
+                titulo={tar.titulo}
+                setor={tar.setor}
+                descricao={tar.descricao}
+                />
+            ))}
+        </DivLista>
   )
 }
