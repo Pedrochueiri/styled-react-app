22244e994dcd0fe75096853f53480ed8878a08d7
pedrochueiri <pedrochu99@hotmail.com>
2022-09-15 08:15:08 -0300

    a

--- rm93939/react-aula7/src/App.jsx
+++ rm93939/react-aula7/src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import ListaTarefa from './components/listaTarefa/ListaTarefa'
 import './index.css'
 
 export default function App() {
   return (
-    <h1>Estudo de Styled Components</h1>
+    <>
+        <h1>ESTUDO SOBRE STYLED-COMPONENTS</h1>
+
+        <ListaTarefa/>
+
+    </>
   )
 }
