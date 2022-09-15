f9512617fa1eba8fd19ed445713300e8a99ac344
Isa Mary <isa.kusuki@gmail.com>
2022-09-14 21:39:34 -0300

    Usado userState e props para atribuir valores nas tarefas

--- rm93307/react-aula7/src/components/listaTarefas/ListaTarefas.jsx
+++ rm93307/react-aula7/src/components/listaTarefas/ListaTarefas.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
 import Tarefa from '../tarefa/Tarefa';
 import { DivLista } from '../../style/styled';
+import { useState } from 'react';
 
 export default function ListaTarefas() {
+
+    const [tarefa, setTarefa] = useState([
+      {
+      titulo: 'Lista de Pagamento',
+      setor: 'Dep. Vendas',
+      descricao: 'Levantar os valores das vendas',
+      },
+      {
+        titulo: 'Testes DEV',
+        setor: 'Tecnologia',
+        descricao: 'Testar aplicação',
+      },
+      {
+        titulo: 'Design Página Web',
+        setor: 'UX',
+        descricao: 'Fazer o design da home',
+      },
+    ])
+
   return(
     <DivLista>
-      <p>Lista de Tarefas</p>
-      <Tarefa/>
-      <Tarefa/>
-      <Tarefa/>
+      {tarefa.map((tar,i)=>(
+        <Tarefa
+        key={i}
+        titulo={tar.titulo}
+        setor={tar.setor}
+        descricao={tar.descricao}
+        />
+      ))}
     </DivLista>
   )
 }
